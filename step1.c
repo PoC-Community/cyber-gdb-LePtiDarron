7716e141221e1449322dbbd49664001dcccd5eb0
Victor-Garreau <victor.garreau@conscio-technologies.com>
2025-11-20 19:31:21 +0100

    ADD step1 and first flag of step 2

---
+++ step1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int A = 7;
+    int B = 1;
+
+    if (A + B == 8)
+        puts("Normal.");
+    else
+        puts("Hmm wait what ?!");
+
+    return B;
+}
